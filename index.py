a55fde4e91b7b02f67b91cb912a23ccea3bcc989
LimitToInfinity <21stLimitless@gmail.com>
2020-08-25 23:10:04 -0600

    Major refactor.

--- index.py
+++ index.py
@@ -1,40 +1,41 @@
 import time
+import random
 
-name = raw_input("What is your name? ")
+name = raw_input('What is your name? ')
 
-print "Hello,", name + "! Time to play hangman!"
-
+print 'Hello,', name + '! Time to play hangman!'
 time.sleep(1)
-
-print "Start guessing..."
+print 'Start guessing...'
 time.sleep(0.5)
 
-word = "secret"
-
-guesses = ""
-
+words = ['secret', 'forever', 'cruise', 'healthy', 'vegetarian']
+word = random.choice(words)
+guesses = ''
 turns = 10
 
 while turns > 0:
-    failed = 0
+  word_count = len(word)
 
-    for letter in word:
-        if letter in guesses:
-            print letter,
-        else:
-            print "_",
-            failed += 1
+  for letter in word:
+    if letter in guesses:
+      word_count -= 1
+      print letter,
+    else:
+      print '_',
 
-    if failed == 0:
-        print "You won"
-        break
-    
-    guess = raw_input("guess a character: ")
-    guesses += guess
-    if guess not in word:
-        turns -= 1
-        print "No dice!"
-        print "You have", str(turns), "more guesses..."
+  if word_count == 0:
+    print 'You won'
+    break
 
-    if turns == 0:
-        print "Game over!"
+  guess = raw_input('guess a character: ')
+  if guess in guesses:
+    print 'You already guessed ' + guess + '!'
+  elif guess not in word:
+    turns -= 1
+    print 'No dice!'
+    print 'You have', str(turns), 'more guesses...'
+
+  guesses += guess
+
+  if turns == 0:
+    print 'Game over!'
